c008f971f67c50d282624ed605241822beca9a67
Spiess Beat <beat.spiess@adcubum.com>
2019-04-12 16:57:48 +0200

    2 new examples for quizes.

---
+++ src/main/java/quizes/Crito2.java
@@ -0,0 +1,15 @@
+package quizes;
+
+public class Crito2 {
+
+    public static void main(String[] ags) {
+        int x = 16;
+
+        switch(x) {
+            case 020: System.out.println("A");
+            case 016: System.out.println("B");
+            default: System.out.println("C");
+            break;
+        }
+    }
+}
---
+++ src/main/java/quizes/MyOne.java
@@ -0,0 +1,7 @@
+package quizes;
+
+public class MyOne {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
