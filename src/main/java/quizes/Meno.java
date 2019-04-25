6adb43b19b31d73019c4fc63139f6bc56dc5b2b6
Spiess Beat <beat.spiess@adcubum.com>
2019-04-25 17:58:17 +0200

    New quizes Meno and PlusPlus

---
+++ src/main/java/quizes/Meno.java
@@ -0,0 +1,12 @@
+package quizes;
+
+public class Meno {
+
+    public static void main(String[] args) {
+
+        Boolean b1 = Boolean.valueOf( "True"); //true
+        Boolean b2 = Boolean.valueOf( "1"); //false
+        System.out.println(b1&b2); //false
+    }
+
+}
---
+++ src/main/java/quizes/PlusPlus.java
@@ -0,0 +1,16 @@
+package quizes;
+
+public class PlusPlus {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i<1; i = iplusplus(i)) {
+            System.out.println("t1");
+        }
+    }
+
+    private static int iplusplus(int i) {
+        System.out.println("t2");
+        return ++i;
+    }
+}
